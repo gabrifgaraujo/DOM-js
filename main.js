8abf24bcd6165857d58112a312f86f9a3da4dd5b
Priscilla-Marinho2005 <priscillamarinho2005@gmail.com>
2025-09-15 11:56:11 -0300

    correção de erros no button removeLastItem

--- main.js
+++ main.js
@@ -1,76 +1,55 @@
 const Itens = []
-
-function update(){
-    const nomeItens = document.getElementById("nome")
-    const regiaoItens = document.getElementById("regiao")
-    const novaLinha = document.createElement("tr")
-    const novaColunaNome = document.createElement("td")
-    const novaColunaRegiao = document.createElement("td")
-    const valorNome = nomeItens.value
-    const valorRegiao = regiaoItens.value
-    let antigoNome
-    let antigaRegiao
-    novaColunaNome.textContent = valorNome
-    novaLinha.className = "odd:bg-gray-900 even:bg-gray-800 border-gray-700"
-    novaColunaNome.className = "px-6 py-4"
-    novaColunaRegiao.className = "px-6 py-4"
-    novaColunaRegiao.textContent = valorRegiao
-    novaColunaNome.id = "nomeItemAdicionado"
-    novaColunaRegiao.id = "regiaoItemAdicionado"
-    novaLinha.id = "novaLinhaAdicionada"
-    document.querySelector("table").appendChild(novaLinha)
-    novaLinha.appendChild(novaColunaNome)
-    novaLinha.appendChild(novaColunaRegiao)
-
-    Itens.push([valorNome,valorRegiao])
-    antigoNome = valorNome
-    antigaRegiao = valorRegiao
-    console.log(Itens)
-
-    for(let i=0;i<=Itens.length;i++){
-        nomesLista.remove()
-        regiaoLista.remove()
-    }
-    console.log("lista atualizada")
-
-    
-}
-
+let contadorID = 1
 function addElement(){
+    let antigoNome
+    let antigaRegiao
     const nomeItens = document.getElementById("nome")
     const regiaoItens = document.getElementById("regiao")
     const novaLinha = document.createElement("tr")
+    let novaLinhaID = document.createElement("td")
     const novaColunaNome = document.createElement("td")
     const novaColunaRegiao = document.createElement("td")
     const valorNome = nomeItens.value
     const valorRegiao = regiaoItens.value
-    let antigoNome
-    let antigaRegiao
+
     novaColunaNome.textContent = valorNome
     novaLinha.className = "odd:bg-gray-900 even:bg-gray-800 border-gray-700"
+    novaLinhaID.className = "px-6 py-4"
     novaColunaNome.className = "px-6 py-4"
     novaColunaRegiao.className = "px-6 py-4"
     novaColunaRegiao.textContent = valorRegiao
+
+    novaLinhaID.textContent = contadorID
+    contadorID++
+    novaLinhaID.id = "novoIDadicionado"
+    // if(novaLinhaID){
+    //     novaLinhaID.id = "antigoIDadicionado"
+    // }else if(!novaLinhaID){
+    //     novaLinha.id = "ultimoIDadicionado"
+    // }
+
     novaColunaNome.id = "nomeItemAdicionado"
     novaColunaRegiao.id = "regiaoItemAdicionado"
     novaLinha.id = "novaLinhaAdicionada"
+
     document.querySelector("table").appendChild(novaLinha)
+    novaLinha.appendChild(novaLinhaID)
     novaLinha.appendChild(novaColunaNome)
     novaLinha.appendChild(novaColunaRegiao)
 
-    Itens.push([valorNome,valorRegiao])
-    antigoNome = valorNome
+    Itens.push([contadorID-1,valorNome,valorRegiao])
+    antigoNome = valorNome 
     antigaRegiao = valorRegiao
     console.log(Itens)
 }
 
 function removeLastItem(){
-    Itens.pop()
     console.log(Itens)
-    const nomesLista = document.getElementById("nomeItemAdicionado")
-    const regiaoLista = document.getElementById("regiaoItemAdicionado")
-    nomesLista.remove()
-    regiaoLista.remove()
+    const linhaAtual = novaLinhaAdicionada[novaLinhaAdicionada.length-1]
+    linhaAtual.remove()
+    Itens.pop()
+    console.log(contadorID)
+    contadorID-1
 }
 
 function clearList(){
@@ -78,16 +57,18 @@ function clearList(){
         if(Itens.length < 1){
             break
         }else {
-            const antigaLinha = document.getElementById("novaLinhaAdicionada")
-            antigaLinha.remove()
+            const linhaAtual = document.getElementById("novaLinhaAdicionada")
+            linhaAtual.remove()
             Itens.pop()
+            contadorID = 1
         }
     }
     console.log(Itens) 
 }
 
-function searchElement(){
-    for(let i=0;i<Itens[i];i++){
-        console.log(Itens[i])
+function update(){
+    for(let i=0;i<Itens.length;i++){
+        console.log(Itens)
+        const linhaCriada = document.getElementById("novaLinhaAdicionada")
     }
 }
